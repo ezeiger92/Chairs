980a7d13f03fcdbc1fc03b57f45acc4c43d78525
ezeiger92 <ezeiger92@gmail.com>
2020-01-01 15:36:46 -0500

    Reference new command API

--- src/main/java/com/chromaclypse/chairs/Chairs.java
+++ src/main/java/com/chromaclypse/chairs/Chairs.java
@@ -1,10 +1,12 @@
 package com.chromaclypse.chairs;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.chromaclypse.api.command.CommandBase;
+import com.chromaclypse.api.command.Context;
 
 public class Chairs extends JavaPlugin {
 	private InteractListener inter;
@@ -12,7 +14,15 @@ public class Chairs extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		inter = new InteractListener(this);
-		getCommand("chairs").setExecutor(this);
+		
+		TabExecutor ch = new CommandBase()
+				.calls(this::helpCommand)
+				.with().arg("reload").calls(this::reloadCommand)
+				.with().arg("version").calls(CommandBase::pluginVersion)
+				.getCommand();
+
+		getCommand("chairs").setExecutor(ch);
+		getCommand("chairs").setTabCompleter(ch);
 		this.getServer().getPluginManager().registerEvents(inter, this);
 		this.getServer().getPluginManager().registerEvents(new DoubleDoor(), this);
 	}
@@ -26,23 +36,14 @@ public class Chairs extends JavaPlugin {
 		}
 	}
 	
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String alias, String[] parameters) {
-		if(parameters.length == 0) {
-			sender.sendMessage("Chairs: /"+alias+" reload");
-		}
-		else {
-			switch(parameters[0]) {
-				case "reload":
-					inter.config.init(this);
-					sender.sendMessage("Chairs: Reloaded config!");
-					break;
-
-				default:
-					sender.sendMessage("Chairs: /"+alias+" reload");
-					break;
-			}
-		}
+	private boolean helpCommand(Context context) {
+		context.Sender().sendMessage("Chairs: /"+context.Alias()+" reload");
+		return true;
+	}
+	
+	private boolean reloadCommand(Context context) {
+		inter.config.init(this);
+		context.Sender().sendMessage("Chairs: Reloaded config!");
 		return true;
 	}
 }
